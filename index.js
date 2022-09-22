8eab6c6ce5d234fd60a25152d1b751b17b8a23bf
paullaster <paullasterokoth98@gmail.com>
2022-09-22 05:15:20 +0300

    adding a getter method

--- index.js
+++ index.js
@@ -2,14 +2,16 @@
  * PRICE CALCULATOR
  */
 class MenuItemPriceCalculator {
-    #lapton_price = 700;
+    #laptop_price = 700;
     #phone_price = 500;
     constructor (laptop, phone) {
         this.laptop = laptop;
         this.phone  = phone;
     }
     purchaseTotal () {
-        
+        return ( (this.laptop * this.#laptop_price) + (this.phone * this.#phone_price));
     }
-
+    get totalPrice () {
+        return this.purchaseTotal ();
+    }
 };
