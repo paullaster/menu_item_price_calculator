2cd3ef4a9174149b153581f18c69232aae4db8d2
paullaster <paullasterokoth98@gmail.com>
2022-09-22 05:06:39 +0300

    adding class

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * PRICE CALCULATOR
+ */
+class MenuItemPriceCalculator {
+    #lapton_price;
+    #phone_price;
+    constructor (lapton_price, phone_price) {
+        this.#lapton_price = lapton_price;
+        this.#phone_price = phone_price
+    }
+};
